eaa2d7fbd1e522e551b3d28ea75c9dc897e631d0
thom8047 <thom8047@bears.unco.edu>
2021-08-04 13:45:41 -0600

    Added in database connectivity

--- app.js
+++ app.js
@@ -1,24 +1,82 @@
+/* ---------------------------------------------------------------------------------- */
+// Variables 
+
 var express = require('express'),
     port = 8000,
     app = express(),
-    pg = require('pg');
+    pg = require('pg'),
+    db = {
+        user: 'editordb',
+        host: 'localhost',
+        database: 'testdb_editor',
+        password: 'testeditor',
+        port: 5432,
+    };
 
-app.use(express.static(__dirname + '/client')); //__dir and not _dir
-app.listen(port);
+/* ---------------------------------------------------------------------------------- */
+// App setup 
 
-const client = new pg.Client({
-    user: 'dbuser',
-    host: 'localhost',
-    database: 'mydb',
-    password: 'secretpassword',
-    port: 3211,
-});
+app.use(express.json())                             // so we can parse data
+app.use(express.static(__dirname + '/client'));     // to the client folder
+app.listen(port);                                   // what port we're using
 
-client.connect()
+/* ---------------------------------------------------------------------------------- */
+// Pool and Client connections
 
-client.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    client.end()
+/*
+dbuser: editordb
+dbpass: testeditor
+*/
+
+const pool = new pg.Pool(db)
+// the pool will emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
 })
+
+/* ---------------------------------------------------------------------------------- */
+// Functionality for server
+
+function getDBName(_obj, result) {
+    var obj = JSON.parse(_obj);
+    console.log(result)
+
+    for (let dict_of_names of result) {
+        // check if names match! we should not have any conflicting names, duplicates, or any weird occurances!
+        if (obj["name"] === dict_of_names["name"]) {
+            // now we have full row data from db:  | name | file_no | content | accessed |
+
+            return dict_of_names["content"];
+        }
+    }
+    return false;
+}
+
+/* ---------------------------------------------------------------------------------- */
+// App post, get, update and delete
+
+app.post('/data', function(request, response){
+	var obj = JSON.stringify(request.body), // name | file_no
+        found = false;
+
+    pool.connect().then(client => {
+        client.query('SELECT * FROM data_holdings')
+        .then(result => {
+            client.release()
+            var content = getDBName(obj, result.rows)
+            if (content) {
+                response.send(content);
+            }
+            
+        })
+        .catch(error => {
+            client.release()
+            response.send("Error * ");
+            console.log('There was an issue: ', error) 
+        });
+    });
+});
 
 console.log(`Serving on port: ${port}`)
